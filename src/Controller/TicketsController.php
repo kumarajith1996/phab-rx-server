17c41c7abedc9ef8e72a00eaf1434bd2d22a456b
Ajith Kumar <ajith.kumar@zoomrx.com>
2020-12-18 17:47:17 +0530

    Fix Tickets Fetch

--- src/Controller/TicketsController.php
+++ src/Controller/TicketsController.php
@@ -21,24 +21,42 @@ class TicketsController extends AppController
     public function index()
     {
         $result = ConduitHelper::callMethodSynchronous('maniphest.search', ['attachments' => ['projects' => true], "limit"=> 10]);
-        $owners = ConduitHelper::callMethodSynchronous('user.search', ['attachments' => ['projects' => true], "limit"=> 10]);
-        $projects = ConduitHelper::callMethodSynchronous('maniphest.search', ['attachments' => ['projects' => true], "limit"=> 10]);
         $selectedOwners = [];
         $selectedProjects = [];
         foreach ($result['data'] as $project) {
-            $users = [];
+            $selectedOwners[] = $project['fields']['ownerPHID'] ?? '';
+            $selectedProjects = array_merge($selectedProjects, $project['attachments']['projects']['projectPHIDs']);
+        }
+        // $selectedProjects = array_unique($selectedProjects);
+        $projects = ConduitHelper::callMethodSynchronous('project.search', ['constraints' => ['phids' => $selectedProjects]]);
+        $owners = ConduitHelper::callMethodSynchronous('user.search', ['constraints' => ['phids' => $selectedOwners]]);
+        $ownerMap = [];
+        $projectMap = [];
+        foreach ($owners['data'] as $owner) {
+            $ownerMap[$owner['phid']] = $owner['fields']['username'];
+        }
+        foreach ($projects['data'] as $project) {
+            $projectMap[$project['phid']] = $project['fields']['name'];
         }
         $returnData = [];
+        Log::debug($result);
+        Log::debug($ownerMap);
+        Log::debug($projectMap);
         foreach ($result['data'] as $project) {
-            $returnData[] = [
+            $currentData = [
                 'id' => $project['id'],
                 'phid' => $project['phid'],
                 'name' => $project['fields']['name'],
                 'description' => $project['fields']['description']['raw'],
+                'owner' => $ownerMap[$project['fields']['ownerPHID']] ?? '',
                 'ownerPHID' => $project['fields']['ownerPHID'],
                 'status' => $project['fields']['status']['name'],
                 'priority' => $project['fields']['priority']['name']
             ];
+            foreach ($project['attachments']['projects']['projectPHIDs'] as $phid) {
+                $currentData['projects'][] = ['phid' => $phid, 'name' => $projectMap[$phid]];
+            }
+            $returnData[] = $currentData;
         }
 
         $this->set(compact('returnData'));
